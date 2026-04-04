7f7dcd48fffa53be13ca20590a91bc3fae204304
AdHam <adhamahmed1144@gmail.com>
2026-04-04 13:52:40 +0200

    Making closures

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const first = () => {
-  second();
-  console.log("first");
-};
-const second = () => {
-  third();
-  console.log("second");
-};
-const third = () => {
-  console.log("third");
+const makeSize = (size) => {
+  return () => {
+    document.body.style.fontSize = `${size}px`;
+  };
 };
 
-first();
+const size12 = makeSize(12);
+const size14 = makeSize(14);
+const size16 = makeSize(16);
+
+document.getElementById("size-12").onclick = size12;
+document.getElementById("size-14").onclick = size14;
+document.getElementById("size-16").onclick = size16;
